7557e6c74c2c1f02c8d13cc96543a596980d582a
Yogs <yogazacty@gmail.com>
2018-07-03 14:59:03 +0700

    yogs

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import requests, json
 app = Flask(__name__)
 
 @app.route('/hello') #contoh
@@ -17,3 +18,12 @@ def akhir():
 @app.route('/alwan/', methods=['GET'])
 def alwan():
     return "<h1>alwan suryansah 1164033 mengerjakan quiz.</p>"
+
+@app.route('/<username>', methods=['GET'])
+def instagram(username):
+    uri = 'https://apinsta.herokuapp.com/u/'+username
+    response = app.response_class(
+        response=requests.get(uri).text,
+        mimetype='application/json')
+    return response
+    
