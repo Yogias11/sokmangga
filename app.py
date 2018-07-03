0f726412808ffeb8d3adfbc4e966c9030c6453e1
Dinda Ayu Pratiwi <dindaayuprtw@gmail.com>
2018-07-03 15:01:21 +0700

    menambahkan quis

--- app.py
+++ app.py
@@ -17,3 +17,7 @@ def akhir():
 @app.route('/alwan/', methods=['GET'])
 def alwan():
     return "<h1>alwan suryansah 1164033 mengerjakan quiz.</p>"
+
+@app.route('/dinda/', methods=['GET'])
+def dinda():
+    return "<h1>METHODE GET BY DINDA AYU PRATIWI</h1>"
